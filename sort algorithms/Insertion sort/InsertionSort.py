76cf93be9eb0a18fe55337437dd4bcd4a74faa11
yosefther <yousefthair9@gmail.com>
2025-09-14 19:15:59 +0300

    add Insertion sort algorithm

---
+++ sort algorithms/Insertion sort/InsertionSort.py
@@ -0,0 +1,14 @@
+# Input :  A sequence of n numbers <a1 ,a2 , ...  , an>
+#Output : a permutation (reordering) <a1` ,a2` , ... , an`> such that  [a1'<= a2' <= ...<=a2`]
+#  The numbers that we wish to sort are also known as the "keys"
+# 
+#  
+
+A = (2,3,1,6,5,4)
+key = 0
+for i in range(len(A)):
+    key = A[i]
+    while A[i] > key :
+        A[i+1] = A[i] 
+              
+#unsolved
