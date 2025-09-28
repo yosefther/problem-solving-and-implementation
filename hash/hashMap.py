9ccd4e4bcd9e0031dcc3fbebb25f4058b3fa6497
yosefther <yousefthair9@gmail.com>
2025-09-28 15:38:33 +0300

    Hash Implementation

--- hash/hashMap.py
+++ hash/hashMap.py
@@ -1,40 +1,52 @@
-import sys
-
-class pair:
-    def __init__(self , key =0  , val =0  ):
-        self.key = key 
+class Pair:
+    def __init__(self , key , val):
         self.val = val
+        self.key = key
 
 class HashMap:
-    def __init__(self , key = 0  , val = 0  ):
-        self.map=[None]
-        self.pair = pair (key , val)
-        self.key = key
-    def hash(self):
-        for c in self.pair.key:
-            index = ord(c)%sys.getsizeof(self.map)
-        self.key = index
-        print (self.key)
-        return self.key
+    def __init__(self):
+        self.size = 2
+        self.capasity = 2
+        self.map=[None,None]
+        
+    def _hash(self,key):
+        index = 0
+        for c in key:
+            index += ord(c)
+        print(index % self.capasity)
+        return index % self.capasity
 
-    def put(self,key,val):
-        if self.map[self.hash(key)] ==  None:
-            self.map[self.hash(key)] = pair(key , val) 
-        else:
-            k = 0 
-            while self.map[self.hash(key)] != None:
-                i+=i
-                k = self.hash(key)+i
-            self.map[k] = pair(key,val)
-        # print(self.map[self.hash()].__dict__) >> for test
-    
-    # def get (self , key):
-    #     self .
-    #     pass 
+    def _rehash(self):
+        self.capasity = 2 *self.capasity
+        newMap = []
+        for i in range(newMap):
+            newMap.append(None)
+        oldMap = self.map
+        self.map = newMap
+        self.size() =0 
+        for pair in oldMap :
+            if pair:
+                self.put(pair.key , pair.val)
+  
+    def put (self, key , val):
+        pair = Pair(key , val)
+        index = self._hash(key)
+        while True : 
+            if self.map[index] == None:
+                self.map[index] = pair() 
+                if self.size>=self.capasity:
+                    self._rehash()
+            elif self.map[index].key == key:
+                return pair.key
+            index = 1
+            index = index%self.capasity
 
 
-test = HashMap("","") 
-test.hash()
-
-
+    def get(self,key):
+        index = self._hash(key)
+        while self.map[index] != None:
+            if self.map[index] == key :
+                return self.map[index].val
+            index+=1
+            index = index%self.capasity # * 
 
